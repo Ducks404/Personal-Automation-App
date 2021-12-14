00d3b0f88acd1aeef176a4663ec1b47bef99e1f6
Ducks404 <aghakoeswantoro@gmail.com>
2021-12-14 11:17:36 +0700

    Changed shutil.move to os.rename

--- downloadtoschool.py
+++ downloadtoschool.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 def main(exclude = []):
     target = 'C:/Users/Hp/Documents/dewa_stuff/School/'
@@ -10,9 +9,9 @@ def main(exclude = []):
     files = os.listdir("./")
 
     for file in files:
-        if file.startswith('Dean Agha') and os.path.isfie(file) and file not in exclude:
+        if file.startswith('Dean Agha') and os.path.isfile(file) and file not in exclude:
             print('found a school file')
-            shutil.move(f'{home}{file}', f'{target}{file}')
+            os.rename(f'{home}{file}', f'{target}{file}')
 
 if __name__ == '__main__':
     main()
