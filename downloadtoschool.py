9a723bd719260b5698bbde6640a20b51a83a3b6d
Ducks404 <aghakoeswantoro@gmail.com>
2021-12-14 11:20:17 +0700

    Added comments

--- downloadtoschool.py
+++ downloadtoschool.py
@@ -1,17 +1,19 @@
 import os
 
 def main(exclude = []):
+    # Specifying source and target
+    source = "C:/Users/Hp/Downloads/"
     target = 'C:/Users/Hp/Documents/dewa_stuff/School/'
-    home = "C:/Users/Hp/Downloads/"
 
-    os.chdir(home)
+    os.chdir(source)
 
     files = os.listdir("./")
 
     for file in files:
+        # Moving files that start with Dean Agha
         if file.startswith('Dean Agha') and os.path.isfile(file) and file not in exclude:
             print('found a school file')
-            os.rename(f'{home}{file}', f'{target}{file}')
+            os.rename(f'{source}{file}', f'{target}{file}')
 
 if __name__ == '__main__':
     main()
